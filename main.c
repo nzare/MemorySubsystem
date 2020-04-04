2219340ef7c12bb0307d51a506d645dcde3241df
Nidhi Zare <40881094+nzare@users.noreply.github.com>
2020-04-04 11:08:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,16 +7,16 @@
 #include<stdint.h>
 
 typedef struct va_process{
-	uint32_t va;
-	uint8_t process_no;
+  uint32_t va;
+  uint8_t process_no;
 }va_process;
 
 void get_input_va(char* filesList[],int n){
-	
-	int file_pos[n];
+  
+  int file_pos[n];
 
     for(int i=0;i<n;i++)
-    	file_pos[i]=0;
+      file_pos[i]=0;
 
     int num_completed=0;
     int curr_file=0;
@@ -26,50 +26,54 @@ void get_input_va(char* filesList[],int n){
     }
 
     while(1){
-    	
+      
       //FILE *fp;
-		  //fp = fopen(filesList[curr_file],"r"); // or this
+      //fp = fopen(filesList[curr_file],"r"); // or this
 
-  		//struct va_process call_function; //maybe this was causing the problem
+      struct va_process call_function; //maybe this was causing the problem
 
-   		for(int i=0;i<200;i++){
+      for(int i=0;i<200;i++){
 
-   			if(file_pos[curr_file]==-1){
-   				break;
-   			}
+        if(file_pos[curr_file]==-1){
+          break;
+        }
 
-   			char line[9];
+        char line[9];
 
-   			fseek( fp[curr_file], file_pos[curr_file], SEEK_SET );
-   			
-   			if(fgets(line,9, fp[curr_file])==NULL){
-   				//printf("File completed\n");
-   				num_completed+=1;
-   				file_pos[curr_file]=-1;
-   				break;
-   			}
+        fseek( fp[curr_file], file_pos[curr_file], SEEK_SET );
+        
+        if(fgets(line,9, fp[curr_file])==NULL){
+          //printf("File completed\n");
+          num_completed+=1;
+          file_pos[curr_file]=-1;
+          break;
+        }
 
-   			file_pos[curr_file]+=(10);
-   			
-   			printf("%s : %d\n",line,curr_file);
+        file_pos[curr_file]+=(10);
+        
+        printf("%s : %d\n",line,curr_file);
 
-   			unsigned int h;
-   			sscanf(line, "%x", &h);
+        unsigned int h;
+        sscanf(line, "%x", &h);
 
-   			//can be used to call other functions
-   			//We have 32 bit unsigned integer for address and 8 bit int for processno
+        //can be used to call other functions
+        //We have 32 bit unsigned integer for address and 8 bit int for processno
 
-   			//call_function.va=h;
-   			//call_function.process_no=curr_file;
-   		}
-   		
-   		curr_file=(curr_file+1)%n;
-   		if(num_completed==n){
-   			return;
-   		}
+        call_function.va=h;
+        call_function.process_no=curr_file;
+      }
+      
+      curr_file=(curr_file+1)%n;
+      if(num_completed==n){
+        for (int i = 0; i < n; ++i)
+        {
+            fclose(fp[i]);
+        }
+        return;
+      }
     }
 
-	
+  
 
 }
 
@@ -100,12 +104,12 @@ int main(int argc, char *argv[])
 
     rewinddir(dir);
 
-	char *filesList[num_input];
-	int i=0;
+  char *filesList[num_input];
+  int i=0;
 
-	//Put file names into the array
-	while ((entry = readdir(dir)) != NULL){
-    	if (entry->d_type != DT_REG)
+  //Put file names into the array
+  while ((entry = readdir(dir)) != NULL){
+      if (entry->d_type != DT_REG)
             continue;
 
         pos = strlen(entry->d_name) - 4;
@@ -114,23 +118,23 @@ int main(int argc, char *argv[])
         {
 
             filesList[i] = (char*) malloc (strlen(entry->d_name)+1);
-        	strncpy (filesList[i],entry->d_name, strlen(entry->d_name) );
-        	filesList[i][strlen(entry->d_name)]='\0';
-        	i++;
+          strncpy (filesList[i],entry->d_name, strlen(entry->d_name) );
+          filesList[i][strlen(entry->d_name)]='\0';
+          i++;
         }
-	}
-	if (closedir(dir) == -1)
+  }
+  if (closedir(dir) == -1)
     {
         perror("closedir");
         return 1;
     }
 
 
-	get_input_va(filesList,num_input);
-	for(i=0;i<num_input;i++){
-		printf("%s",filesList[i] );
+  get_input_va(filesList,num_input);
+  /*for(i=0;i<num_input;i++){
+    printf("%s",filesList[i] );
 
-	}
-	
-	return 0;
+  }*/
+  
+  return 0;
 }
